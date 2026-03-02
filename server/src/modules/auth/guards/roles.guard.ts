456c3c58d159b2f25c4167f56d9b582701c225fc
Danny Simba <dannysimba2602@gmail.com>
2026-03-02 18:18:33 -0500

    delete injectable

--- server/src/modules/auth/guards/roles.guard.ts
+++ server/src/modules/auth/guards/roles.guard.ts
@@ -3,28 +3,4 @@ import { Reflector } from '@nestjs/core';
 
 import { UserRole } from '@prisma/client';
 
-// @Injectable() Metodo de roles Prueba
-// export class RolesGuard implements CanActivate {
-//   constructor(private readonly reflector: Reflector) {}
-
-//   canActivate(context: ExecutionContext): boolean {
-//     const roles = this.reflector.get<UserRole[]>(
-//       'roles',
-//       context.getHandler(),
-//     );
-
-//     if (!roles) {
-//       return true;
-//     }
-
-//     const request = context.switchToHttp().getRequest();
-//     const user = request.user;
-
-//     if (!user) {
-//       return false;
-//     }
-
-//     return roles.some((role) => user.role === role);
-//   }
-// }
 
